9aadb2b744361b8ab7b4066fba11ce1bdf9e6265
Mrigangka Datta <mrigangkadatta15@gmail.com>
2024-03-05 17:43:03 +0530

    some changes

---
+++ currencyproject/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-slate-800 font-semibold">Currency project</h1>
+    </>
+  );
+}
+
+export default App;
